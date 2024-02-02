e8083108a2786cb8c1de4ca968ccc6d8090c188f
persi <jina8151010@gmai.com>
2024-02-02 16:01:37 +0900

    Feature: 게시글 목록 controller

--- src/main/java/persi/makeboard/board/controller/BoardController.java
+++ src/main/java/persi/makeboard/board/controller/BoardController.java
@@ -2,9 +2,12 @@ package persi.makeboard.board.controller;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 import persi.makeboard.board.dto.BoardDto;
 import persi.makeboard.board.service.BoardService;
+
+import java.util.List;
 
 @Controller
 @RequiredArgsConstructor
@@ -21,5 +24,12 @@ public class BoardController {
         System.out.println("boardDto = " + boardDto);
         boardService.save(boardDto);
         return "index";
+    }
+    @GetMapping("/")
+    public String findAll(Model model){ //데이터를 가져올때 model 객체 사용
+        // DB에서 전체 게시글 데이터를 가져와서 list.html에 보여준다
+        List<BoardDto> boardDtoList = boardService.findAll();
+        model.addAttribute("boardList", boardDtoList);
+        return "list";
     }
 }
