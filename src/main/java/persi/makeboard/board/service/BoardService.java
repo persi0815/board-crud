b00f8fcc78c75e966967661ffb05061eb2dabee6
persi <jina8151010@gmai.com>
2024-02-02 16:03:26 +0900

    Feature: boardDtoList 만들기

--- src/main/java/persi/makeboard/board/service/BoardService.java
+++ src/main/java/persi/makeboard/board/service/BoardService.java
@@ -6,6 +6,9 @@ import persi.makeboard.board.domain.Board;
 import persi.makeboard.board.dto.BoardDto;
 import persi.makeboard.board.repository.BoardRepository;
 
+import java.util.ArrayList;
+import java.util.List;
+
 // Dto(controller에서 받음) -> Entity(repository로 넘겨줌): Entity class
 // Entity(repository에서 받음) -> Dto(controller로 넘겨줌): Dto class
 @Service
@@ -15,5 +18,14 @@ public class BoardService {
     public void save(BoardDto boardDto) {
         Board board = Board.toSaveEntity(boardDto);
         boardRepository.save(board);
+    }
+
+    public List<BoardDto> findAll() {
+        List<Board> boardList = boardRepository.findAll();
+        List<BoardDto> boardDtoList = new ArrayList<>();
+        for(Board board: boardList) {
+            boardDtoList.add(BoardDto.toBoardDto(board));
+        }
+        return boardDtoList;
     }
 }
