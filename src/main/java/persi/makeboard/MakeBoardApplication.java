a871e5b66e441662ce4c0b2429b600616ca846e9
persi <jina8151010@gmai.com>
2024-02-02 13:07:56 +0900

    Feature: 초기 설정

---
+++ src/main/java/persi/makeboard/MakeBoardApplication.java
@@ -0,0 +1,12 @@
+package persi.makeboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MakeBoardApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(MakeBoardApplication.class, args);
+	}
+
+}
---
+++ src/test/java/persi/makeboard/MakeBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package persi.makeboard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MakeBoardApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
