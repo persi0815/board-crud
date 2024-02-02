734524d33fcc7c12075b4776e0a5ed7a5e07c488
persi <jina8151010@gmai.com>
2024-02-02 18:04:36 +0900

    Feature: 게시글 수정 service

--- src/main/java/persi/makeboard/board/service/BoardService.java
+++ src/main/java/persi/makeboard/board/service/BoardService.java
@@ -49,4 +49,11 @@ public class BoardService {
             return boardDto;
         } else return null;
     }
+
+    // 게시물 수정
+    public BoardDto update(BoardDto boardDto) {
+        Board board = Board.toUpdateEntity(boardDto);
+        boardRepository.save(board);
+        return findById(boardDto.getId());
+    }
 }
