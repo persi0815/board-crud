b21ec803a83b077f6060c5b526b94932de65748b
persi <jina8151010@gmai.com>
2024-02-06 13:58:33 +0900

    Feature: 첨부파일 요청 보내기

--- src/main/java/persi/makeboard/board/dto/BoardDto.java
+++ src/main/java/persi/makeboard/board/dto/BoardDto.java
@@ -1,6 +1,7 @@
 package persi.makeboard.board.dto;
 
 import lombok.*;
+import org.springframework.web.multipart.MultipartFile;
 import persi.makeboard.board.domain.Board;
 
 import java.time.LocalDateTime;
@@ -20,6 +21,12 @@ public class BoardDto {
     private int boardHits;
     private LocalDateTime boardCreatedTime;
     private LocalDateTime boardUpdatedTime;
+
+    private MultipartFile boardFile; // save.thml -> controller 파일 담는 용도
+    private String originalFileName; // 원본 파일 이름
+    private String storedFileName; // 서버 저장용 파일 이름 -> 파일명 중복문제 해소
+    private int fileAttached; // 파일 첨부 여부(첨부 1, 미첨부 0)
+
 
     public BoardDto(Long id, String boardWriter, String boardTitle, int boardHits, LocalDateTime boardCreatedTime) {
         this.id = id;
